8a5a0ff1609e632875edde0c0318bc2fb8e5e8d7
tom_2 <test2@practise.com>
2022-03-10 11:20:13 +0800

    添加文件

---
+++ maven_git/src/main/java/com/practise/maven/HelloGit2.java
@@ -0,0 +1,13 @@
+package com.practise.maven;
+
+/**
+ * <Description>  <br>
+ *
+ * @author wang.guanghui01 <br>
+ * @vertion 9.0E <br>
+ * @taskId <br>
+ * @CreateDate 2022/3/10 <br>
+ * @see com.practise.maven <br>
+ **/
+public class HelloGit2 {
+}
